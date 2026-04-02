e386ac45918185594a884f1fa7d851b27892929e
Jagadeeshmopuri <32411712+Jagadeeshmopuri@users.noreply.github.com>
2026-04-02 14:50:13 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+function bookService(service) {
+  const name = prompt("Enter your name:");
+  const location = prompt("Enter your location:");
+
+  alert(`Booking Confirmed for ${service}\nName: ${name}\nLocation: ${location}`);
+
+  // Later we connect backend here
+}
